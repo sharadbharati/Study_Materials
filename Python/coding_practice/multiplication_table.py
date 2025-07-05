312b3bd0170fe3edd2cf76ee646da22c4f82573d
Sharad Bharati <sharadbharati.sb@gmail.com>
2025-07-05 19:01:24 +0530

    multiplication table

--- Python/coding_practice/multiplication_table.py
+++ Python/coding_practice/multiplication_table.py
@@ -1,5 +1,12 @@
-n = 5
-
-for i in range(1, 11): 
-    print(n*i)
-    
+def multiplication_table(n):
+    i = 1 
+    while i <= 10: 
+    # for i in range(1, 11): 
+        # print(n, "x", i, "=", n*i) 
+        print(f"{n} X {i} = {n*i}")  # f-string
+        # print("{} X {} = {}".format(n, i, n*i))
+        i = i + 1
+        
+# n = int(input()) 
+n = input("Enter Number: ")  
+multiplication_table(int(n))
